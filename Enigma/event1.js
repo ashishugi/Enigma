1ede13475d8cd646912de8cb238ead82b460603d
ashishugi <ashishkumarguptacse@gmail.com>
2019-10-10 00:20:29 +0530

    added register2.html , register2.css , register2.js

---
+++ Enigma/event1.js
@@ -0,0 +1,3 @@
+function tohome(){
+    window.location.href= "event.html";
+}
---
+++ Enigma/register2.js
@@ -0,0 +1,18 @@
+i=0;
+function clicked(x){
+ console.log(x);    
+    
+   if(document.getElementById(x).checked == true){
+       i++;
+       if(i>4){
+           alert("cannot take part in more than 4 events");
+           i--;
+        document.getElementById(x).checked = false;
+       }
+      
+   }
+   else if(document.getElementById(x).checked == false){
+    i--; 
+   }
+
+}
